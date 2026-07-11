c5430f6764b987d655b49e5d166c27f3fb33f5f0
nrxarise-cyber <nrxarise@gmail.com>
2026-07-11 11:08:45 +0530

    chore: create component export index for easier imports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,32 @@
+// Layout Components
+export { PageLayout } from './layout/PageLayout'
+export { PremiumHeader } from './layout/PremiumHeader'
+export { PremiumSection } from './layout/PremiumSection'
+export { PremiumBottomNav } from './layout/PremiumBottomNav'
+
+// UI Components
+export { GlassCard } from './ui/GlassCard'
+export { GlowEffect } from './ui/GlowEffect'
+export { PremiumInput } from './ui/PremiumInput'
+export { PremiumTextarea } from './ui/PremiumTextarea'
+export { PremiumButton } from './ui/PremiumButton'
+export { PremiumBadge } from './ui/PremiumBadge'
+export { StatusIndicator } from './ui/StatusIndicator'
+export { RiskScore } from './ui/RiskScore'
+export { AnimatedCounter } from './ui/AnimatedCounter'
+
+// Feature Components - IP
+export { IPCheckerForm } from './features/ip/IPCheckerForm'
+export { IPResultCard } from './features/ip/IPResultCard'
+
+// Feature Components - Proxy
+export { ProxyCheckerForm } from './features/proxy/ProxyCheckerForm'
+export { ProxyResultCard } from './features/proxy/ProxyResultCard'
+
+// Feature Components - Bulk
+export { BulkCheckerForm } from './bulk/BulkCheckerForm'
+export { BulkResultList } from './bulk/BulkResultList'
+
+// History Components
+export { HistoryList } from './history/HistoryList'
+export { HistoryPage } from './history/HistoryPage'
